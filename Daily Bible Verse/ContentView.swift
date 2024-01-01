d20f75b12db115e25ecbe7dcaf803aa308ef288d
Michael Cheng <cheng.c.mike@gmail.com>
2024-01-01 17:25:47 -0600

    initial commit

---
+++ Daily Bible Verse/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Daily Bible Verse
+//
+//  Created by Michael Cheng on 1/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Daily Bible Verse/Daily_Bible_VerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_Bible_VerseApp.swift
+//  Daily Bible Verse
+//
+//  Created by Michael Cheng on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Daily_Bible_VerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
